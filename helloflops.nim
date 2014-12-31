6f9e0d9b3eec3ebdcbb7da9a0ac9d80908a9fa16
def <dennis@felsin9.de>
2014-12-31 02:31:13 +0100

    Initial commit

---
+++ helloflops.nim
@@ -0,0 +1,40 @@
+# Compile with:
+# nim -d:release --gcc.exe:icc --gcc.linkerexe:icc --passC:-O3 --passC:-fopenmp --passC:-mmic --passL:-fopenmp --passL:-mmic c helloflops
+import times
+
+proc omp_get_num_threads(): int {.header: "<omp.h>".}
+proc omp_set_num_threads(int) {.header: "<omp.h>".}
+
+const
+  flopsArraySize = 1024*1024
+  maxFlopsIter = 100_000_000
+  loopCount = 128
+  flopsPerCalc = 2
+
+var fa {.codegenDecl: "$# $# __attribute__((align(64)))".}: array[flopsArraySize, float32]
+var fb {.codegenDecl: "$# $# __attribute__((align(64)))".}: array[flopsArraySize, float32]
+
+let a: float32 = 1.1
+var numThreads: int
+
+echo "Initializing"
+#ompSetNumThreads(4)
+for i in 0 || < flopsArraySize:
+  if i == 0: numThreads = ompGetNumThreads()
+  fa[i] = float32(i) + 0.1
+  fb[i] = float32(i) + 0.2
+
+echo "Starting Compute on ", numThreads, " threads"
+let tstart = epochTime()
+for i in 0 || < numThreads:
+  let offset = i * loopCount
+  for j in 0 .. < maxFlopsIter:
+    for k in 0 .. < loopCount:
+      fa[k+offset] = a * fa[k+offset] + fb[k+offset]
+let tstop = epochTime()
+let ttime = tstop - tstart
+let gflops = 1.0e-9 * float32(numThreads) * loopCount * maxFlopsIter * flopsPerCalc
+
+if ttime > 0:
+  echo "GFlops = ", gflops, ", Secs = ", ttime
+  echo "GFlops per sec = ", gflops/ttime
---
+++ hellomem.nim
@@ -0,0 +1,39 @@
+# Compile with:
+# nim -d:release --gcc.exe:icc --gcc.linkerexe:icc --passC:-O3 --passC:-fopenmp --passC:-mmic --passL:-fopenmp --passL:-mmic c hellomem
+import times
+
+proc omp_get_num_threads(): int {.header: "<omp.h>".}
+proc omp_set_num_threads(int) {.header: "<omp.h>".}
+
+const
+  bwArraySize = 1024*1024*64
+  bwIters = 1000
+  opsPerIter = 2
+
+var fa {.codegenDecl: "$# $# __attribute__((align(64)))".}: array[bwArraySize, float64]
+var fb {.codegenDecl: "$# $# __attribute__((align(64)))".}: array[bwArraySize, float64]
+var fc {.codegenDecl: "$# $# __attribute__((align(64)))".}: array[bwArraySize, float64]
+
+let a: float64 = 1.1
+var numThreads: int
+
+echo "Initializing"
+#ompSetNumThreads(4)
+for i in 0 || < bwArraySize:
+  numThreads = ompGetNumThreads()
+  fa[i] = float64(i) + 0.1
+  fb[i] = float64(i) + 0.2
+  fc[i] = float64(i) + 0.2
+
+echo "Starting BW Test on on ", numThreads, " threads"
+let tstart = epochTime()
+for i in 0 .. < bwIters:
+  for k in 0 || < bwArraySize:
+      fa[k] = fb[k];
+let tstop = epochTime()
+let ttime = tstop - tstart
+let gbytes = 1.0e-9 * opsPerIter * bwIters * bwArraySize * float(sizeof(float64))
+
+if ttime > 0:
+  echo "GBytes = ", gbytes, ", Secs = ", ttime
+  echo "GBytes per sec = ", gbytes/ttime
